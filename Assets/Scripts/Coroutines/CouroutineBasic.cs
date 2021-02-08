827db59afc1e25bcf6446ca5177f6590c37ad38a
Björn <Bjorn.oudkerk@outlook.com>
2021-02-08 12:02:42 +0100

    Opdracht 2

---
+++ Assets/Scripts/Coroutines/CouroutineBasic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CouroutineBasic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Routine");
+    }
+
+    IEnumerator Routine()
+    {
+        Debug.Log("I'm starting the Coroutine!");
+        yield return new WaitForSeconds(.5f);
+        Debug.Log("I'm updating the Coroutine!");
+        yield return new WaitForSeconds(.5f);
+        Debug.Log("I'm ending the Coroutine!");
+    }
+}
