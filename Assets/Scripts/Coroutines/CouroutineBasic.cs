5980a4918700df28bf4336da98703994b0f4db19
Björn <Bjorn.oudkerk@outlook.com>
2021-02-09 22:39:44 +0100

    Opdracht 4

--- Assets/Scripts/Coroutines/CouroutineBasic.cs
+++ Assets/Scripts/Coroutines/CouroutineBasic.cs
@@ -4,20 +4,63 @@ using UnityEngine;
 
 public class CouroutineBasic : MonoBehaviour
 {
+    [Range(0.0f, 10.0f)]
+    public float fadeTime;
+    private bool isFading;
+    private bool isFaded;
+
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine("Routine");
+        isFaded = false;
     }
 
-    IEnumerator Routine()
+    void Update()
     {
-        Debug.Log("I'm starting the Coroutine!");
-        for (float ft = 10; ft > 0; ft--)
+        if (Input.GetKeyDown(KeyCode.Space) && !isFading)
         {
-            Debug.Log("I'm Updating the Coroutine!");
-            yield return new WaitForSeconds(0.5f);
+            switch (isFaded)
+            {
+                case true:
+                StartCoroutine("FadeIn");
+                break;
+
+                case false:
+                StartCoroutine("FadeOut");
+                break;
+            }
         }
-        Debug.Log("I'm ending the Coroutine!");
+    }
+
+    IEnumerator FadeOut()
+    {
+        isFading = true;
+        for (float ft = 1f; ft >= 0; ft -= 0.01f)
+        {
+            Color c = GetComponent<Renderer>().material.color;
+            c.a = ft;
+            GetComponent<Renderer>().material.color = c;
+            Debug.Log("I'm fading out");
+            yield return new WaitForSeconds(fadeTime/100);
+        }
+        Debug.Log("JOBS DONE");
+        isFading = false;
+        isFaded = true;
+    }
+
+    IEnumerator FadeIn()
+    {
+        isFading = true;
+        for (float ft = 0f; ft <= 1; ft += 0.01f)
+        {
+            Color c = GetComponent<Renderer>().material.color;
+            c.a = ft;
+            GetComponent<Renderer>().material.color = c;
+            Debug.Log("I'm fading in");
+            yield return new WaitForSeconds(fadeTime/100);
+        }
+        Debug.Log("JOBS DONE");
+        isFading = false;
+        isFaded = false;
     }
 }
